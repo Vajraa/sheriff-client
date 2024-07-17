cbaecdddbfcb8611c1e0021539eb6760310e4fe9
Aman Kumar <97329195+amanksh05@users.noreply.github.com>
2024-07-18 02:11:22 +0530

    navbar modification

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,13 +4,13 @@ export default function Navbar() {
     return <>
         <nav className="flex justify-between items-baseline border-b pb-4 border-quartz">
             <div className="flex gap-6 md:gap-16 justify-between">
-                <h1 id="logo">Sheriff</h1>
-                <div className="flex gap-5">
+                <h1 id="logo" className="font-serif">Sheriff</h1>
+                <div className="flex gap-8">
                     <a href="/doc" className="hover:underline underline-offset-8">Docs</a>
                     <a href="/blogs" className="hover:underline underline-offset-8">Blog</a>
                 </div>
             </div>
-            <button className="bg-smoky-black hover:opacity-70 transition-opacity border-ghost-white border flex justify-center gap-2 px-6 py-3 rounded-xl shadow-inner shadow-gray-500">
+            <button className="bg-smoky-black hover:opacity-70 transition-opacity border-ghost-white border flex justify-center items-center gap-3 px-8 py-3 rounded-md shadow-inner shadow-gray-500">
                 <span>Login with</span>
                 <Image src={github} width={18} height={18} alt="Github" />
             </button>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import patch1 from './assets/patch-1.svg'
 import patch2 from './assets/patch-2.svg'
 export default function Home() {
   return (
-    <main className="flex min-h-screen relative flex-col justify-between p-4 md:p-16 bg-chinese-black">
+    <main className="flex min-h-screen relative flex-col justify-between p-4  bg-chinese-black">
       <Navbar/>
       <Image className="absolute left-0 top-0 cursor-none pointer-events-none" src={patch1} alt="glow" width={555} height={590}/>
       {/* <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
